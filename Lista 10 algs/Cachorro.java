e6bbb5b39aa808150748fecf5e75016210186ffe
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-05-28 19:38:39 -0300

    Create Cachorro.java

---
+++ Lista 10 algs/Cachorro.java
@@ -0,0 +1,49 @@
+public class Cachorro{
+    private String nome;
+    private int idade;
+    private String raca;
+
+    public Pessoa(String nome, int idade, String raca){
+        this.nome = nome;
+        this.idade = idade;
+        this.raca = raca;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getRaca(){
+        return raca;
+    }
+    public void setCpf(String raca){
+        this.raca = raca;
+    }
+
+    public void latir(){
+        System.out.println("AUAUAUAUAU");
+    }
+    public void comer(){
+        System.out.println("HMMMMM YUUM");
+    }
+    public void dormir(){
+        System.out.println("ZZZ..");
+    }
+}
+
+class App{
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro("Rex", 7, "Beagle");
+
+        System.out.println(cachorro.getNome() + " está ");
+        cachorro.latir();
+    }
+}
